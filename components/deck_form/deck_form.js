0d4fda1525f7708a2f05be647bf36bdf1fa3a8d3
Davin Kim <davinkim2015@u.northwestern.edu>
2016-09-22 11:24:07 -0700

    Got the language picker up and running, and the request body seems to set properly. Still need to hook up this body with the YANDEX translate API. Still some minor troubles with ScrollView, but we'll figure that out as we go. Still need to implement the feature for adding variable number of cards.

--- components/deck_form/deck_form.js
+++ components/deck_form/deck_form.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import * as _ from 'lodash';
-import { Text, TextInput, View } from 'react-native';
+import {
+	Text,
+	TextInput,
+	View,
+	StyleSheet,
+	PickerIOS,
+	ScrollView,
+	Dimensions
+} from 'react-native';
 import Button from 'react-native-button';
+
+const LANGUAGES = {
+	Chinese: "zh",
+	English: "en",
+	French: "fr",
+	Japanese: "ja",
+	Korean: "ko",
+	Portuguese: "pt",
+	Spanish: "es"
+};
 
 class DeckForm extends React.Component {
 	constructor(props) {
 		super(props);
 		this.deleteFirstDeck = this.deleteFirstDeck.bind(this);
 		let deck = new Array(5).fill("");
-		this.state = {deck, title: '', to: 'ko'};
+		this.state = {deck, title: '', to: 'Chinese', height: 650};
 	}
 
 	updateText(text, idx) {
@@ -23,26 +41,36 @@ class DeckForm extends React.Component {
 		this.setState({title});
 	}
 
+	updateTo(language) {
+		this.setState({to: language});
+	}
+
 	deleteFirstDeck(e) {
 		e.preventDefault();
 		this.props.deleteDeck(this.props.decks[0]._id);
 	}
 
 	_handlePress() {
-		this.props.submitDeck(this.state);
+		const requestBody = {
+			deck: this.state.deck,
+			title: this.state.title,
+			to: LANGUAGES[this.state.to]
+		};
+
+		this.props.submitDeck(requestBody);
 	}
 
 
 	render() {
 		let title = (
 			<View>
-				<Text>Title</Text>
+				<Text style={styles.deckTitle}>Title</Text>
 				<TextInput
-					style={{height: 40}}
-					placeholder="TITLE PLZ"
+					style={styles.titleInput}
+					placeholder="Title of Deck"
 					onChangeText={text => this.updateTitle(text)}
 					/>
-				</View>
+			</View>
 
 		);
 
@@ -62,24 +90,53 @@ class DeckForm extends React.Component {
 
 		});
 
-
 		return(
-			<View style={{marginTop: 50}}>
-				{title}
-				<Button onPress={this.props.redirectToDecksIndex}>Back to Decks</Button>
-				<Button onPress={this.deleteFirstDeck}>Delete First Deck</Button>
-				{deck}
-				<Button
-				style={{fontSize: 20, color: '#607d8b'}}
-				styleDisabled={{color: 'red'}}
-				onPress={() => this._handlePress()}>
-				Press Me!
-			</Button>
-			</View>
+				<ScrollView
+					contentContainerStyle={{marginLeft: 15, flex: 1, height: this.state.height}}>
+					{title}
+					<PickerIOS
+						selectedValue={this.state.to}
+						onValueChange={(language) => this.updateTo(language)}>
+						{Object.keys(LANGUAGES).map((language) => (
+							<PickerIOS.Item
+								key={language}
+								value={language}
+								label={language}
+							/>
+						))}
+					</PickerIOS>
+					{deck}
+					<Button
+					style={{fontSize: 20, color: '#607d8b'}}
+					styleDisabled={{color: 'red'}}
+					onPress={() => this._handlePress()}>
+						Press Me!
+					</Button>
+					<Button onPress={this.deleteFirstDeck}>Delete First Deck</Button>
+				</ScrollView>
 		);
 	}
 }
 
-
+const styles = StyleSheet.create({
+	form: {
+		marginLeft: 15,
+		flex: 1
+	},
+	deckTitle: {
+		fontSize: 16,
+		padding: 3,
+		color: "#4891C0"
+	},
+	titleInput: {
+		flex: 1,
+		height: 40,
+		padding: 5,
+		width: 250,
+		borderWidth: 1,
+		borderColor: "#ccc",
+		borderRadius: 5
+	}
+});
 
 export default DeckForm;
